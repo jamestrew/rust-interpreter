482496b44686d4bc8092ec3257dedca51898f288
James Trew <j.trew10@gmail.com>
2023-07-31 21:52:15 -0400

    start repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io::{self, BufReader, Stdin, Stdout};
+
+use rust_interpreter::lexer::Lexer;
+
+const PROMPT: &'static str = ">> ";
+
+fn main() -> io::Result<()> {
+    println!("Time to monkey around");
+    repl(io::stdin(), io::stdout())
+}
+
+fn repl(stdin: Stdin, stdout: Stdout) -> io::Result<()> {
+    let mut reader = BufReader::new(stdin);
+    let mut line = String::new();
+
+    loop {
+        print!("{}", PROMPT);
+        stdout.lock().flush().unwrap();
+        reader.read_line(&mut line).unwrap();
+
+        let lexer = Lexer::new(&line);
+        for token in lexer {
+            println!("{:?}", token);
+        }
+
+        line.clear();
+    }
 }
