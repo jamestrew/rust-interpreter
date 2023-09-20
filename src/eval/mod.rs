a78e959ba238f4cc100aca183412cb678528daea
James Trew <j.trew10@gmail.com>
2023-09-19 22:17:27 -0400

    complete infix evaluation

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -4,25 +4,35 @@ use crate::ast::*;
 use crate::lexer::Token;
 
 pub enum Object {
-    Int { value: i64 },
-    Bool { value: bool },
-    StringLiteral { value: String },
+    Int(i64),
+    Bool(bool),
+    StringLiteral(String),
     Null,
 }
 
-const TRUE: Object = Object::Bool { value: true };
-const FALSE: Object = Object::Bool { value: false };
+impl Object {
+    fn new_bool(val: bool) -> Object {
+        if val {
+            TRUE
+        } else {
+            FALSE
+        }
+    }
+}
 
 impl Display for Object {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Object::Int { value } => write!(f, "{}", value),
-            Object::Bool { value } => write!(f, "{}", value),
-            Object::StringLiteral { value } => write!(f, "{}", value),
+            Object::Int(value) => write!(f, "{}", value),
+            Object::Bool(value) => write!(f, "{}", value),
+            Object::StringLiteral(value) => write!(f, "{}", value),
             Object::Null => write!(f, "null"),
         }
     }
 }
+
+const TRUE: Object = Object::Bool(true);
+const FALSE: Object = Object::Bool(false);
 
 pub fn eval_program(program: &Program) -> Object {
     if let Some(stmt) = program.statements.first() {
@@ -39,9 +49,7 @@ fn eval_expression(expr: &Expression) -> Object {
     match expr {
         Expression::Identifier(_val) => todo!(),
         Expression::Primative(val) => eval_primative(val),
-        Expression::StringLiteral(val) => Object::StringLiteral {
-            value: val.to_string(),
-        },
+        Expression::StringLiteral(val) => Object::StringLiteral(val.to_string()),
         Expression::Prefix(val) => eval_prefix(val),
         Expression::Infix(val) => eval_infix(val),
         Expression::If(_val) => todo!(),
@@ -50,8 +58,8 @@ fn eval_expression(expr: &Expression) -> Object {
 
 fn eval_primative(expr: &Primative) -> Object {
     match expr {
-        Primative::Int(val) => Object::Int { value: *val },
-        Primative::Bool(val) => Object::Bool { value: *val },
+        Primative::Int(val) => Object::Int(*val),
+        Primative::Bool(val) => Object::new_bool(*val),
     }
 }
 
@@ -66,23 +74,19 @@ fn eval_prefix(expr: &Prefix) -> Object {
 fn eval_bang_prefix(right: &Expression) -> Object {
     let right = eval_expression(right);
     let truthy = match right {
-        Object::Int { value } => value != 0,
-        Object::Bool { value } => value,
-        Object::StringLiteral { value } => !value.is_empty(),
+        Object::Int(value) => value != 0,
+        Object::Bool(value) => value,
+        Object::StringLiteral(value) => !value.is_empty(),
         Object::Null => false,
     };
 
-    if truthy {
-        FALSE
-    } else {
-        TRUE
-    }
+    Object::new_bool(!truthy)
 }
 
 fn eval_minus_prefix(right: &Expression) -> Object {
     let right = eval_expression(right);
-    if let Object::Int { value } = right {
-        return Object::Int { value: -value };
+    if let Object::Int(value) = right {
+        return Object::Int(-value);
     }
     unreachable!("ERROR: found non-integer value in unary operator `-`");
 }
@@ -90,64 +94,50 @@ fn eval_minus_prefix(right: &Expression) -> Object {
 fn eval_infix(expr: &Infix) -> Object {
     let left = eval_expression(expr.left());
     let right = eval_expression(expr.right());
+    let op = expr.operator();
 
-    match expr.operator() {
-        Token::Minus => eval_infix_minus(&left, &right),
-        Token::Plus => eval_infix_plus(&left, &right),
-        Token::Asterisk => eval_infix_multiply(&left, &right),
-        Token::ForwardSlash => eval_infix_divide(&left, &right),
-        Token::Equal => todo!(),
-        Token::NotEqual => todo!(),
-        Token::LT => todo!(),
-        Token::GT => todo!(),
-        _ => expr.unreachable_operator(),
+    match (&left, &right) {
+        (Object::Int(left), Object::Int(right)) => eval_integer_infix(*left, *right, &op),
+        (Object::StringLiteral(left), Object::StringLiteral(right)) => {
+            eval_string_infix(&left, &right, &op)
+        }
+        (Object::Bool(left), Object::Bool(right)) => eval_bool_infix(*left, *right, &op),
+        (Object::Int(left), Object::Bool(right)) => eval_bool_infix(*left != 0, *right, &op),
+        (Object::Bool(left), Object::Int(right)) => eval_bool_infix(*left, *right != 0, &op),
+        _ => todo!("eval_infix: handle rest as errors?"),
     }
 }
 
-fn eval_infix_minus(left: &Object, right: &Object) -> Object {
-    let (left, right) = integer_infix_values(left, right).unwrap();
-    Object::Int {
-        value: left - right,
+fn eval_integer_infix(left: i64, right: i64, operator: &Token) -> Object {
+    match operator {
+        Token::Minus => Object::Int(left - right),
+        Token::Plus => Object::Int(left + right),
+        Token::Asterisk => Object::Int(left * right),
+        Token::ForwardSlash => Object::Int(left / right), // HACK: this could panic implicity via
+        // divide by zero
+        Token::Equal => Object::new_bool(left == right),
+        Token::NotEqual => Object::new_bool(left != right),
+        Token::LT => Object::new_bool(left < right),
+        Token::GT => Object::new_bool(left > right),
+        _ => Prefix::unreachable_operator(),
     }
 }
 
-fn eval_infix_plus(left: &Object, right: &Object) -> Object {
-    if let (Object::Int { value: left }, Object::Int { value: right }) = (left, right) {
-        return Object::Int {
-            value: *left + *right,
-        };
-    } else if let (Object::StringLiteral { value: left }, Object::StringLiteral { value: right }) =
-        (left, right)
-    {
-        return Object::StringLiteral {
-            value: format!("{}{}", left, right),
-        };
-    }
-    panic!("Operands of `+` must both either be Object::StringLiteral or Object::Int")
-}
-
-fn eval_infix_multiply(left: &Object, right: &Object) -> Object {
-    let (left, right) = integer_infix_values(left, right).unwrap();
-    Object::Int {
-        value: left * right,
+fn eval_string_infix(left: &str, right: &str, operator: &Token) -> Object {
+    match operator {
+        Token::Plus => Object::StringLiteral(format!("{}{}", left, right)),
+        Token::Equal => Object::new_bool(left == right),
+        Token::NotEqual => Object::new_bool(left != right),
+        _ => Prefix::unreachable_operator(),
     }
 }
 
-fn eval_infix_divide(left: &Object, right: &Object) -> Object {
-    let (left, right) = integer_infix_values(left, right).unwrap();
-    // HACK: this could panic implicitly
-    Object::Int {
-        value: left / right,
+fn eval_bool_infix(left: bool, right: bool, operator: &Token) -> Object {
+    match operator {
+        Token::Equal => Object::new_bool(left == right),
+        Token::NotEqual => Object::new_bool(left != right),
+        _ => Prefix::unreachable_operator(),
     }
-}
-
-fn integer_infix_values(left: &Object, right: &Object) -> anyhow::Result<(i64, i64)> {
-    if let (Object::Int { value: left }, Object::Int { value: right }) = (left, right) {
-        return Ok((*left, *right));
-    }
-    Err(anyhow::anyhow!(
-        "Both operands of must be of type Object::Int"
-    ))
 }
 
 #[cfg(test)]
@@ -200,14 +190,34 @@ mod test {
     assert_stmt!(prefix_minus_integer_2, "-(-1)", "1");
 
     assert_stmt!(infix_integer_1, "5 + 5 + 5 + 5 - 10", "10");
-    // assert_stmt!(infix_integer_2, "2 * 2 * 2 * 2 * 2", "32");
-    // assert_stmt!(infix_integer_3, "-50 + 100 + -50", "0");
-    // assert_stmt!(infix_integer_4, "5 * 2 + 10", "20");
-    // assert_stmt!(infix_integer_5, "5 + 2 * 10", "25");
-    // assert_stmt!(infix_integer_6, "20 + 2 * -10", "0");
-    // assert_stmt!(infix_integer_7, "50 / 2 * 2 + 10", "60");
-    // assert_stmt!(infix_integer_8, "2 * (5 + 10)", "30");
-    // assert_stmt!(infix_integer_9, "3 * 3 * 3 + 10", "37");
-    // assert_stmt!(infix_integer_10, "3 * (3 * 3) + 10", "37");
-    // assert_stmt!(infix_integer_11, "(5 + 10 * 2 + 15 / 3) * 2 + -10", "50");
+    assert_stmt!(infix_integer_2, "2 * 2 * 2 * 2 * 2", "32");
+    assert_stmt!(infix_integer_3, "-50 + 100 + -50", "0");
+    assert_stmt!(infix_integer_4, "5 * 2 + 10", "20");
+    assert_stmt!(infix_integer_5, "5 + 2 * 10", "25");
+    assert_stmt!(infix_integer_6, "20 + 2 * -10", "0");
+    assert_stmt!(infix_integer_7, "50 / 2 * 2 + 10", "60");
+    assert_stmt!(infix_integer_8, "2 * (5 + 10)", "30");
+    assert_stmt!(infix_integer_9, "3 * 3 * 3 + 10", "37");
+    assert_stmt!(infix_integer_10, "3 * (3 * 3) + 10", "37");
+    assert_stmt!(infix_integer_11, "(5 + 10 * 2 + 15 / 3) * 2 + -10", "50");
+    assert_stmt!(infix_integer_12, "1 < 2", "true");
+    assert_stmt!(infix_integer_13, "1 > 2", "false");
+    assert_stmt!(infix_integer_14, "1 < 1", "false");
+    assert_stmt!(infix_integer_15, "1 > 1", "false");
+    assert_stmt!(infix_integer_16, "1 == 1", "true");
+    assert_stmt!(infix_integer_17, "1 != 1", "false");
+    assert_stmt!(infix_integer_18, "1 == 2", "false");
+    assert_stmt!(infix_integer_19, "1 != 2", "true");
+
+    assert_stmt!(infix_bool_1, "true == true", "true");
+    assert_stmt!(infix_bool_2, "false == false", "true");
+    assert_stmt!(infix_bool_3, "true == false", "false");
+    assert_stmt!(infix_bool_4, "true != false", "true");
+    assert_stmt!(infix_bool_5, "false != true", "true");
+    assert_stmt!(infix_bool_6, "(1 < 2) == true", "true");
+    assert_stmt!(infix_bool_7, "(1 < 2) == false", "false");
+    assert_stmt!(infix_bool_8, "(1 > 2) == true", "false");
+    assert_stmt!(infix_bool_9, "(1 > 2) == false", "true");
+
+    assert_stmt!(infix_string, "\"foo\" + \"bar\"", "foobar");
 }
